d423dfbc4ee10c35d2ccc3a954601a45fc5357ca
KMorrison00 <kylem.workemail@gmail.com>
2022-09-05 21:44:29 -0700

    hello world

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Layout, Header} from './Layout.components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header> Card Game Knuckle HEads</Header>
+      <Layout>
+        Hello World
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
