5102d26cac43830f30e45dd330e52c0aa7bc5343
KMorrison00 <kylem.workemail@gmail.com>
2022-10-14 23:15:24 -0700

    rule touch ups for clarity

--- src/RulesModal.js
+++ src/RulesModal.js
@@ -39,8 +39,9 @@ export function RulesModal({setShowModal, showModal}) {
                                 The game ends when one player fills their entire 3x3 grid, then total scores are compared
                                 Each card has its displayed point value except for face cards which are worth 5.
                                 Scores are calculated first by summing the column of a grid and then applying multipliers.
-                                When a card is played, if the opponent has a card of the same value in the opposing column it will 
-                                be removed
+
+                                When a card is played, if the opponent has a card or multiple cards of the same value in the
+                                opposing column it will be removed. Cards that are removed do not get reshuffled into the deck
                             </p>
                         </div>
                     </div>
@@ -61,7 +62,7 @@ export function RulesModal({setShowModal, showModal}) {
                         <div className="flex flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Straights</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
-                                Straights Apply an extra 2x multiplier
+                                Straights apply an extra 2x multiplier
                             </p>
                         </div>
                     </div>
@@ -73,7 +74,7 @@ export function RulesModal({setShowModal, showModal}) {
                         <img src={images['3C.png']} className="object-cover w-1/2 h-48 rounded-t-lg md:h-auto md:w-24 md:rounded-none md:rounded-l-lg" alt="Card Back" />                        <div className="flex flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Flushes</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
-                                Flushes Apply an extra 2x multiplier
+                                Flushes apply an extra 2x multiplier
                             </p>
                         </div>
                     </div>
@@ -85,7 +86,22 @@ export function RulesModal({setShowModal, showModal}) {
                         <img src={images['9C.png']} className="object-cover w-1/2 h-48 rounded-t-lg md:h-auto md:w-24 md:rounded-none md:rounded-l-lg" alt="Card Back" />                        <div className="flex flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Straight Flushes</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
-                                Striaght Flushes Apply a 6x multiplier
+                                Striaght Flushes apply a 6x multiplier
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative p-6 flex-auto ">
+                    <div className="flex flex-col items-center  rounded-lg border shadow-md md:flex-row md:max-w-xl border-gray-700 bg-gray-800">
+                        <img src={images['8H.png']} className="object-cover w-1/2 h-48 rounded-t-lg md:h-auto md:w-24 md:rounded-none md:rounded-l-lg" alt="Card Back" />
+                        <img src={images['8C.png']} className="object-cover w-1/2 h-48 rounded-t-lg md:h-auto md:w-24 md:rounded-none md:rounded-l-lg" alt="Card Back" />
+                        <img src={images['8S.png']} className="object-cover w-1/2 h-48 rounded-t-lg md:h-auto md:w-24 md:rounded-none md:rounded-l-lg" alt="Card Back" />                        <div className="flex flex-col justify-between p-4 leading-normal">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Doubles and Triples</h5>
+                            <p className="mb-3 font-normal text-left text-gray-300">
+                                Doubles and triples grant multipliers only to the cards that match.
+                                Two 8's are worth (8 + 8) * 2 = 32. If another single card is in the column
+                                its value will just be added on top for. if it were a 9 it would just 32 + 9. 
+                                Triples just multiply 3x the summed column value 
                             </p>
                         </div>
                     </div>
@@ -103,8 +119,7 @@ export function RulesModal({setShowModal, showModal}) {
                         <div className="flex flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Aces</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
-                                Aces are able to mimic any card value that is in the opposing column.
-                                This means that you can remove any value from the opposing column with an ace
+                                Aces can remove any value in the corresponding opponents column.
                                 They are still only worth 1.
                             </p>
                         </div>
@@ -117,7 +132,7 @@ export function RulesModal({setShowModal, showModal}) {
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Twos</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
                                 Twos apply 2x multiplier to the column its in. This multiplier is bonus
-                                to the other multipliers like doubles, triples, straights and flushes.
+                                to the other multipliers like pairs, triples, straights and flushes.
                                 They have a base value of 2. 
                             </p>
                         </div>
@@ -130,7 +145,7 @@ export function RulesModal({setShowModal, showModal}) {
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Face Cards</h5>
                             <p className="mb-3 font-normal text-left text-gray-300">
                                 While only worth 5 all face cards have higher multipliers than regular cards 
-                                for pairs and triples. It is 3x for doubles instead of 2x and 5x for triples instead of 3x
+                                for pairs and triples. It is 3x for pairs instead of 2x and 5x for triples instead of 3x
                             </p>
                         </div>
                     </div>
